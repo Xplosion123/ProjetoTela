2784eb79eb2ca87823132f37d3d5d644f81e582f
Henrique <henrimelo06@gmail.com>
2026-05-27 16:18:10 -0300

    Chore:cria o projeto + Banco de dados

---
+++ ProjetoTela/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoTela.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

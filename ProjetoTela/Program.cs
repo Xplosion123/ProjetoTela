2247101d0a91174a85d74fe23345261b2101c8db
Rodrigo <zinxrodrigo01@gmail.com>
2026-05-28 14:23:53 -0300

    feat:Adicionando a tela de login

--- ProjetoTela/Program.cs
+++ ProjetoTela/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options => {
-        options.LoginPath = "/Login/Logar";
+        options.LoginPath = "/Login/Index";
         options.AccessDeniedPath = "/Login/AcessoNegado";
     });
 
